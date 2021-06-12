c13461ab008f611db3abcf7d29da0b69959bac94
amal r <amalr405@gmail.com>
2021-06-12 22:33:42 +0530

    palindrome checker

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Palindrome from './Palindrome';
+ 
+
+ReactDOM.render(
+  < >
+    <Palindrome />
+  </ >,
+  document.getElementById('root')
+);
+
+ 
